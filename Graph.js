0a206fa4c4ac839a6faa483b6bf8ce09f546937f
Taylor Fondren <rtfondren@gmail.com>
2013-03-27 12:41:32 -0400

    Begin process of creating Graph API, added Node and Edge classes

--- Graph.js
+++ Graph.js
@@ -1,6 +1,8 @@
+/* An adjacency matrix based graph implementation */
 function Graph(n) {
-	var that = this;
 	var matrix = new Array(n);
+	var nodes = new Array();
+	var that = this;
 
 	for (var i = 0; i < n; ++i) {
 		matrix[i] = new Array(n);
@@ -9,9 +11,13 @@ function Graph(n) {
 		}
 	}
 
-	this.addEdge = function(n1, n2, weight) {
-		matrix[n1][n2] = weight;
-		matrix[n2][n1] = weight;
+	this.addEdge = function(n1, n2, e) {
+		matrix[n1.getID()][n2.getID()] = matrix[n2.getID()][n1.getID()] =  e.getWeight();
+		e.addNode(n1).addNode(n2);
+	};
+
+	this.addNode = function(n) {
+		nodes.push(n);
 	};
 
 	this.print = function() {
@@ -23,6 +29,47 @@ function Graph(n) {
 	this.getMatrix = function() {
 		return matrix;
 	};
+
+	this.traverse = function(start, nodeFunc, edgeFunc) {
+
+	};
 };
+
+function Node(l) {
+	var label = l;
+	var edges = new Array();
+	var that = this;
+
+	this.getLabel = function() {
+		return label;
+	};
+
+	this.addEdge = function(e) {
+		edges.push(e);
+		return that;
+	};
+
+	this.getEdges = function() {
+		return edges;
+	};
+}
+
+function Edge(w) {
+	var weight = w;
+	var nodes = new Array();
+	var that = this;
+
+	this.getWeight = function() {
+		return w;
+	};
+
+	this.addNode = function(n) {
+		nodes.push(n);
+		return that;
+	};
+
+	this.getNodes = function() {
+		return nodes;
+	};
 
 exports.MyGraph = Graph;
